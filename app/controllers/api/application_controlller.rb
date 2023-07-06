a976061618660b57e8f74f1624a1c55996e13990
100rabhg <sourabhpatware100@gmail.com>
2023-07-06 16:07:17 +0530

    more change

--- app/controllers/api/application_controlller.rb
+++ app/controllers/api/application_controlller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
   skip_before_action :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?
 
