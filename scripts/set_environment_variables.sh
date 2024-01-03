eef5ce7bab6e3f51494ebfaa34510827ab47020e
Sourabh patware <89065033+100rabhg@users.noreply.github.com>
2024-01-03 13:41:55 +0530

    connect RDS

---
+++ scripts/set_environment_variables.sh
@@ -0,0 +1,8 @@
+function set_parameter {
+SSM_PARAM_NAME=$1
+SSM_VALUE=`aws ssm get-parameters — with-decryption — names “${SSM_PARAM_NAME}” — query ‘Parameters[*].Value’ — output text`
+[ “$(eval echo “$”$1)” == “” ] && echo “export ${SSM_PARAM_NAME}=${SSM_VALUE}” >> ~/.bashrc
+}
+set_parameter “DB_USER”
+set_parameter “DB_PASS”
+set_parameter “DB_HOST”
